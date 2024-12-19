85be8092863563165ece857c8ebd2df64cd59b81
shoura279 <ahmedshoura279@gmail.com>
2024-12-20 01:36:31 +0200

    test master

---
+++ src/guards/authentication.ts
@@ -0,0 +1,38 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  HttpException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader?.startsWith('Bearer ')) {
+      throw new BadRequestException('Invalid Bearer key');
+    }
+
+    const token = authHeader.split(process.env.BEARER_KEY)[1];
+
+    if (!token) {
+      throw new BadRequestException('Invalid token');
+    }
+
+    try {
+      const decoded = await this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      request.user = decoded;
+      return true;
+    } catch (error) {
+      throw new HttpException(error.message, 400);
+    }
+  }
+}
